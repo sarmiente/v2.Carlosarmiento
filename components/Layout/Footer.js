1fc5b7e23d79662de4a754cc02e20c4193dafe08
sarmiente <carlosarmientopinto@gmail.com>
2022-08-18 19:26:00 -0500

    2 files modified

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <footer className="">
+    <footer>
       <div className="container mx-auto items-center">
         <div className="flex justify-center space-x-3">
           <a href="" className="hover:-translate-y-1 transition transform">
